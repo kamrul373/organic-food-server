069f2db08e57eee9487c6d33fab0665ffdaae728
unknown <textile.zero@gmail.com>
2022-12-27 09:17:55 +0600

    single product api integrated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbqhd62.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -20,9 +20,21 @@ async function run() {
     const categoriesCollection = client.db("organicfoodshop").collection("categories")
 
     app.get("/products", async (req, res) => {
-        const query = {};
-        const result = await productsCollection.find(query).toArray();
-
+        const request = req.query;
+        // checking if any query sent in request
+        let query = {};
+        let result = [];
+        if (request.limit) {
+            result = await productsCollection.find(query).sort({ name: -1 }).limit(parseInt(request.limit)).toArray();
+        } else {
+            result = await productsCollection.find(query).toArray();
+        }
+        res.send(result)
+    })
+    app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) }
+        const result = await productsCollection.findOne(query)
         res.send(result)
     })
     app.get("/categories", async (req, res) => {
@@ -31,6 +43,8 @@ async function run() {
 
         res.send(result)
     })
+
+
 }
 
 run().catch(error => console.log(error))
